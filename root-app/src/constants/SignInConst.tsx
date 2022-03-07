b38cd1b9ec42f599abf3d801c87f3e8bb5eaad13
luks04 <67431911+luks04@users.noreply.github.com>
2022-03-06 22:55:38 -0500

    Adding Material Design and SignIn template

---
+++ root-app/src/constants/SignInConst.tsx
@@ -0,0 +1,9 @@
+export default {
+    AppName: "Credit Group Technical Challenge App",
+    SignInTitle: "Sign in",
+    ForgotPasswordTitle: "Forgot password?",
+    SignUpTitle: "Don't have an account? Sign Up",
+    RememberMeLabel: "Remember me",
+    EmailLabel: "Email Address",
+    PasswordLabel: "Password"
+} as const
