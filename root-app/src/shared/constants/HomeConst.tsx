412dbff320961bb5534c90077e7adb748dc5f78e
luks04 <67431911+luks04@users.noreply.github.com>
2022-03-09 16:38:28 -0500

    Current exchange rate service integration
    Home component added
    Current exchange rate service implemented

---
+++ root-app/src/shared/constants/HomeConst.tsx
@@ -0,0 +1,8 @@
+export default {
+  AppName: "Credit Group Technical Challenge App",
+  HomeTitle: "Current Exchange Rate service response",
+  SignOutTitle: "Sign out",
+  SignUpTitle: "Don't have an account? Sign Up",
+  FirstActionTitle: "Get Exchange Rate Data",
+  SecondActionTitle: "Upload file",
+} as const;
